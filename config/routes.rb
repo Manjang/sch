e328d088df398f8ff0ae2a8663ea0a00122983b4
ediboss <edigenuis@gmail.com>
2017-11-22 18:44:00 +0000

    Fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root  'hotels#index'
   get	'hotels/index'
   get	'hotels/show'
   post "reviews/create"
