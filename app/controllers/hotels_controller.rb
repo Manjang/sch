8b2b4cd6098a7ad7ce57f271d6f961a88c382f80
Buba Manjang <buba@quest.gm>
2017-11-25 01:58:46 +0000

    Creates the about us page

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -64,6 +64,11 @@ class HotelsController < ApplicationController
 	end
 
 
+	def about
+	    @about = Hotel.where(about: true)
+	end
+
+
 	private
 
 	# def hotel_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get '/hotels/about' => 'hotels#about'
+  resources :hotels
+
   root  'hotels#index'
   get	'hotels/index'
   get	'hotels/show'
